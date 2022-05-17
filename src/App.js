ba38adf289485c5c9325a366c2c0ba6faede21eb
Arek <arkkow98@gmail.com>
2022-05-17 17:23:04 +0200

    adding code sources

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  function pingFunction(){
+    alert('hello react')
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+        >
+          <button onClick={pingFunction}>
+            Naciśnij mnie!
+          </button>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
